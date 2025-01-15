ebd20766f05aa2d4f13dacd8bec18f69de794986
Akash Kennedy <akashkennedy1@gmail.com>
2025-01-16 05:11:52 +0530

    Keymap: Map nh to clear search highlights
    Map nh key to clear search highlights

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 
 -- use jk to exit insert mode
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+--clear search highlights
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
