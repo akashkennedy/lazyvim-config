525f5e8e9a0f72afcba4d5a6ec988bd7a810bc03
Akash Kennedy <akashkennedy1@gmail.com>
2025-01-16 13:25:08 +0530

    Plugins: Add disbled.lua and nvim-cmp
    Added disabled.lua to enable/disable plugins and use nvim-cmp instead of
    blink.cmp

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  -- disable blink.cmp
+  { "saghen/blink.cmp", enabled = false },
+
+  -- enable nvim-cmp instead of blink.cmp
+  { "hrsh7th/nvim-cmp", enabled = true },
+
+  -- disable trouble
+  { "folke/trouble.nvim", enabled = false },
+}
